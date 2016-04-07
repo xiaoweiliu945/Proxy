b04dc0a31a6d5103979cf612f716e2975fe6a88c
xiaoweiliu945 <xiaoweiliu945@gmail.com>
2016-04-07 13:18:25 -0700

    First commit for the project

---
+++ sever.h
@@ -0,0 +1,22 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/sockets.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <openssl/ase.h>
+
+#include <encrypt.h>
+#include <data_structures.h>
+
+
+static void * start_server(const char* mykey, const char* sever_address, int port, int listen_port)
+{
+
+}
+
+
+
+
+
+#endif
